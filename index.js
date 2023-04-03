51bd6af04235377141fe42f4e5d625d80b3c9e53
Himanshu1363 <126802917+Himanshu1363@users.noreply.github.com>
2023-04-03 12:40:43 +0530

    removed bugs
    removed extra code

--- index.js
+++ index.js
@@ -13,25 +13,10 @@ function save(){
     count = 0
 }
 
-let myPoints = 3
-function add3points(){
-    myPoints += 3
-}
-
-console.log(myPoints)
 
 
 
-let err = document.getElementById ("error")
- let a = "something went wrond, please try again"
- function ren()
- {
- err.textContent = a
- }
 
-let firstName = "himanshu"
-let lastName = "sharma"
-let fullName = firstName +" " + lastName
-console.log(fullName)
+
 
 
